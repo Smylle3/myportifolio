b0c76dc32ce7d468da3ff111b87cef4bb62ce4a0
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-05-04 03:21:28 -0300

    Linha vertical colorida

---
+++ src/animation/vertical.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+import ColorMode from 'functions/colorMode'
+export const Vertical = props => {
+    return (
+        <Box
+            w="2px"
+            h="100vh"
+            bgGradient={`linear(to-b, ${ColorMode(
+                'red',
+                'purple'
+            )}, ${ColorMode(
+                'orange',
+                'blue.800'
+            )}, ${ColorMode(
+                'yellow',
+                'blue.500'
+            )}, ${ColorMode(
+                'green',
+                'green'
+            )}, ${ColorMode(
+                'blue.500',
+                'yellow'
+            )}, ${ColorMode(
+                'blue.800',
+                'orange'
+            )}, ${ColorMode(
+                'purple',
+                'red'
+            )})`}
+        ></Box>
+    )
+}
