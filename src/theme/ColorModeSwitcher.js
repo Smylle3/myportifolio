8b390f610322664c47ea421029385c7f6ff3cca3
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-05-04 22:54:31 -0300

    Altera estilo do botão de mudar tema da aplicação

--- src/theme/ColorModeSwitcher.js
+++ src/theme/ColorModeSwitcher.js
@@ -4,22 +4,17 @@ import { FaMoon, FaSun } from 'react-icons/fa'
 
 export const ColorModeSwitcher = props => {
     const { toggleColorMode } = useColorMode()
-    const text = useColorModeValue('dark', 'light')
     const SwitchIcon = useColorModeValue(FaMoon, FaSun)
     return (
         <IconButton
-            size="md"
-            fontSize="lg"
-            aria-label={`Switch to ${text} mode`}
-            variant="ghost"
-            color="current"
-            w={10}
+            variant='github'
             onClick={toggleColorMode}
             icon={<SwitchIcon />}
             _focus={{ outline: 0 }}
             pos="fixed"
             top={1}
             right={1}
+            zIndex={500}
         />
     )
 }
