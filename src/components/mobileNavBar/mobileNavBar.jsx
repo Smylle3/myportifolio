400addce0691a73c8562d4378b096daa7e8954ff
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-05-08 16:00:39 -0300

    Chama o componete de contato do topo quando some

--- src/components/mobileNavBar/mobileNavBar.jsx
+++ src/components/mobileNavBar/mobileNavBar.jsx
@@ -3,35 +3,47 @@ import { Center, Heading, Image } from '@chakra-ui/react'
 import ColorMode from 'functions/colorMode'
 import { Logo } from 'animation/Logo'
 import photo from '../../assets/perfil.jpg'
+import { useInView } from 'react-intersection-observer'
+import Shadenavbar from './shadenavbar'
 
 export default function MobileNavBar() {
+    const { ref, inView } = useInView({
+        threshold: 0,
+    })
     return (
-        <Center
-            flexDirection="column"
-            h="100vh"
-            textAlign="center"
-            backdropFilter="auto"
-            backdropBlur="10px"
-            bgGradient={`linear(to-b, ${ColorMode('white', 'black')}, transparent, transparent)`}
-        >
-            <Logo h="20vmin" />
-            <Heading
-                fontWeight={400}
-                bg={ColorMode('black', 'white')}
-                color={ColorMode('white', 'black')}
-                fontSize={40}
-                marginBottom={10}
+        <>
+            <Center
+                flexDirection="column"
+                h="100vh"
+                textAlign="center"
+                backdropFilter="auto"
+                backdropBlur="10px"
+                bgGradient={`linear(to-b, ${ColorMode(
+                    'white',
+                    'black'
+                )}, transparent, transparent)`}
+                ref={ref}
             >
-                Hi, I'm João Pedro, frontend developer.
-            </Heading>
-            <Image
-                alt="text"
-                src={photo}
-                w="10em"
-                borderRadius="full"
-                border={`3px solid ${ColorMode('white', 'black')}`}
-                boxShadow={`1px 1px 10px -1px ${ColorMode('black', 'black')}`}
-            />
-        </Center>
+                <Logo h="20vmin" />
+                <Heading
+                    fontWeight={400}
+                    bg={ColorMode('black', 'white')}
+                    color={ColorMode('white', 'black')}
+                    fontSize={40}
+                    marginBottom={10}
+                >
+                    Hi, I'm João Pedro, frontend developer.
+                </Heading>
+                <Image
+                    alt="text"
+                    src={photo}
+                    w="10em"
+                    borderRadius="full"
+                    border={`3px solid ${ColorMode('white', 'black')}`}
+                    boxShadow={`1px 1px 10px -1px ${ColorMode('black', 'black')}`}
+                />
+            </Center>
+            <Shadenavbar inView={inView} />
+        </>
     )
 }
