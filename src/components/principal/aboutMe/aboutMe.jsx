83baaf9ef0b048db1a3d54f8856b272cec28881b
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-05-09 13:39:31 -0300

    Faz pequena alteração no texto "sobre mim"

--- src/components/principal/aboutMe/aboutMe.jsx
+++ src/components/principal/aboutMe/aboutMe.jsx
@@ -15,7 +15,7 @@ export default function AboutMe() {
             <Text color="black" fontSize="lg" fontWeight={300} textAlign="justify">
                 Olá, sou João Pedro, também sou conhecido por Sorriso e costumo usar o
                 nickName Smylle em minhas redes. Como o apelido já sugere, sou uma pessoa
-                bem humorada e busco soluções de forma tranquila e em um ritmo leve. Sou
+                bem humorada e busco soluções de forma tranquila e eficaz. Sou
                 desenvolvedor frontend em evolução buscando aprender e me aprofundar mais
                 nessa área que tanto amo. Estou cursando engenharia da computação na
                 Universidade de Brasília, conhecendo cada vez mais os diferentes campos da
