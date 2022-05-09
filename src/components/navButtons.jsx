b69c884d1d80be7c19c4cd72ef99dd1387aa1f45
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-05-08 22:39:32 -0300

    Adiciona novo botão e botão para ocultar grupo

--- src/components/navButtons.jsx
+++ src/components/navButtons.jsx
@@ -1,105 +1,145 @@
 import React from 'react'
-import { IconButton, Flex, Tooltip } from '@chakra-ui/react'
+import { IconButton, Flex, Tooltip, useDisclosure, SlideFade } from '@chakra-ui/react'
 import { RiHomeLine } from 'react-icons/ri'
 import { AiOutlineAppstore, AiOutlineDatabase, AiOutlineControl } from 'react-icons/ai'
 import ColorMode from 'functions/colorMode'
 import { Link, animateScroll as scroll } from 'react-scroll'
 import { ColorModeSwitcher } from 'theme/ColorModeSwitcher'
+import { SiAboutDotMe } from 'react-icons/si'
+import { BiLeftArrow, BiRightArrow } from 'react-icons/bi'
 
-export const NavButtons = props => {
-    function scrollTop() {
-        scroll.scrollToTop()
-    }
+export const NavButtons = () => {
+    const { isOpen, onToggle } = useDisclosure()
+    
     return (
         <Flex direction="column" pos="fixed" zIndex={300} right={2} bottom="10%">
-            <ColorModeSwitcher />
-            <Tooltip
-                label="Home"
-                placement="left"
-                hasArrow
-                bg={ColorMode('white', 'black')}
-                color={ColorMode('black', 'white')}
-            >
-                <IconButton
-                    onClick={() => scrollTop()}
-                    variant="zones"
-                    icon={<RiHomeLine size={20} />}
-                    boxShadow={`1px 1px 3px 1px black`}
-                    _focus={{ outline: 0 }}
-                />
-            </Tooltip>
+            <SlideFade offsetX={50} in={isOpen}>
+                <Flex direction="column">
+                    <ColorModeSwitcher />
+                    <Tooltip
+                        label="Home"
+                        placement="left"
+                        hasArrow
+                        bg={ColorMode('white', 'black')}
+                        color={ColorMode('black', 'white')}
+                    >
+                        <IconButton
+                            onClick={() => scroll.scrollToTop()}
+                            variant="zones"
+                            icon={<RiHomeLine size={20} />}
+                            boxShadow={`1px 1px 3px 1px black`}
+                            _focus={{ outline: 0 }}
+                        />
+                    </Tooltip>
 
-            <Link
-                activeClass="active"
-                to="destaque"
-                spy={true}
-                smooth={true}
-                duration={500}
-                offset={-20}
-            >
-                <Tooltip
-                    label="Destaques"
-                    placement="left"
-                    hasArrow
-                    bg={ColorMode('white', 'black')}
-                    color={ColorMode('black', 'white')}
-                >
-                    <IconButton
-                        variant="zones"
-                        icon={<AiOutlineAppstore size={20} />}
-                        boxShadow={`1px 1px 3px 1px black`}
-                        _focus={{ outline: 0 }}
-                    />
-                </Tooltip>
-            </Link>
+                    <Link
+                        activeClass="active"
+                        to="destaque"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        offset={1}
+                    >
+                        <Tooltip
+                            label="Destaques"
+                            placement="left"
+                            hasArrow
+                            bg={ColorMode('white', 'black')}
+                            color={ColorMode('black', 'white')}
+                        >
+                            <IconButton
+                                variant="zones"
+                                icon={<AiOutlineAppstore size={20} />}
+                                boxShadow={`1px 1px 3px 1px black`}
+                                _focus={{ outline: 0 }}
+                            />
+                        </Tooltip>
+                    </Link>
 
-            <Link
-                activeClass="active"
-                to="projetos"
-                spy={true}
-                smooth={true}
-                duration={500}
-                offset={-20}
-            >
-                <Tooltip
-                    label="Projetos"
-                    placement="left"
-                    hasArrow
-                    bg={ColorMode('white', 'black')}
-                    color={ColorMode('black', 'white')}
-                >
-                    <IconButton
-                        variant="zones"
-                        icon={<AiOutlineDatabase size={20} />}
-                        boxShadow={`1px 1px 3px 1px black`}
-                        _focus={{ outline: 0 }}
-                    />
-                </Tooltip>
-            </Link>
+                    <Link
+                        activeClass="active"
+                        to="about"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        offset={0}
+                    >
+                        <Tooltip
+                            label="Sobre mim"
+                            placement="left"
+                            hasArrow
+                            bg={ColorMode('white', 'black')}
+                            color={ColorMode('black', 'white')}
+                        >
+                            <IconButton
+                                variant="zones"
+                                icon={<SiAboutDotMe size={20} />}
+                                boxShadow={`1px 1px 3px 1px black`}
+                                _focus={{ outline: 0 }}
+                            />
+                        </Tooltip>
+                    </Link>
 
-            <Link
-                activeClass="active"
-                to="skills"
-                spy={true}
-                smooth={true}
-                duration={500}
-                offset={-20}
-            >
-                <Tooltip
-                    label="Skills"
-                    placement="left"
-                    hasArrow
-                    bg={ColorMode('white', 'black')}
-                    color={ColorMode('black', 'white')}
-                >
-                    <IconButton
-                        variant="zones"
-                        icon={<AiOutlineControl size={20} />}
-                        boxShadow={`1px 1px 3px 1px black`}
-                        _focus={{ outline: 0 }}
-                    />
-                </Tooltip>
-            </Link>
+                    <Link
+                        activeClass="active"
+                        to="projetos"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        offset={0}
+                    >
+                        <Tooltip
+                            label="Projetos"
+                            placement="left"
+                            hasArrow
+                            bg={ColorMode('white', 'black')}
+                            color={ColorMode('black', 'white')}
+                        >
+                            <IconButton
+                                variant="zones"
+                                icon={<AiOutlineDatabase size={20} />}
+                                boxShadow={`1px 1px 3px 1px black`}
+                                _focus={{ outline: 0 }}
+                            />
+                        </Tooltip>
+                    </Link>
+
+                    <Link
+                        activeClass="active"
+                        to="skills"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        offset={0}
+                    >
+                        <Tooltip
+                            label="Skills"
+                            placement="left"
+                            hasArrow
+                            bg={ColorMode('white', 'black')}
+                            color={ColorMode('black', 'white')}
+                        >
+                            <IconButton
+                                variant="zones"
+                                icon={<AiOutlineControl size={20} />}
+                                boxShadow={`1px 1px 3px 1px black`}
+                                _focus={{ outline: 0 }}
+                            />
+                        </Tooltip>
+                    </Link>
+                </Flex>
+            </SlideFade>
+            <IconButton
+                colorScheme="red"
+                variant="solid"
+                size="xs"
+                isRound
+                w={5}
+                h={6}
+                _focus={{ outline: 0 }}
+                onClick={onToggle}
+                icon={isOpen ? <BiRightArrow /> : <BiLeftArrow />}
+            />
         </Flex>
     )
 }
