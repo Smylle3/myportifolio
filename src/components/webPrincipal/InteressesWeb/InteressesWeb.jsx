4df524cdc1f9473d1e87070be788a6e2370f5d71
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-05-10 10:13:35 -0300

    Remove linha

--- src/components/webPrincipal/InteressesWeb/InteressesWeb.jsx
+++ src/components/webPrincipal/InteressesWeb/InteressesWeb.jsx
@@ -1,7 +1,6 @@
 import { Box, Heading, ScaleFade } from '@chakra-ui/react'
 import React from 'react'
 import { useInView } from 'react-intersection-observer'
-import Skills from 'components/principal/skills/skills'
 import Interesses from 'components/principal/personal/interesses'
 
 export default function InteressesWeb() {
