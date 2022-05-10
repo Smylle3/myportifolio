fae9f9f15fcd538293652c09d5f5148d6b149be7
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-05-10 10:12:04 -0300

    Muda cores da side bar web

--- src/components/sideBar/components/textGroup.jsx
+++ src/components/sideBar/components/textGroup.jsx
@@ -5,13 +5,13 @@ import React from 'react'
 export default function TextGroup(props) {
     return (
         <Box textAlign="center" margin="10px 0px" w="90%">
-            <Text color={ColorMode('white', 'orange')} fontWeight={700} fontSize={18}>
+            <Text color={ColorMode('black', 'white')} fontWeight={400} fontSize={18}>
                 {props.title}
             </Text>
-            <Text color={ColorMode('black', 'white')} fontSize={15} fontWeight={500}>
+            <Text color={ColorMode('black', 'white')} fontSize={15} fontWeight={300}>
                 {props.content}
             </Text>
-            <Box border={`1px solid ${ColorMode('white', 'orange')}`} marginTop={2} />
+            <Box border={`1px solid ${ColorMode('black', 'white')}`} marginTop={2} />
         </Box>
     )
 }
